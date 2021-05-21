9eecfbc6b33c491693d34771cf5afef441e13703
buttercuprambunctious <bria.stewart08@gmail.com>
2021-05-20 19:53:33 -0700

    commit on 'master;

---
+++ lesson5/js/bom.js
@@ -0,0 +1,25 @@
+
+const input = document.querySelector("input");
+const button = document.querySelector("button");
+const list = document.querySelector("ul");
+
+button.addEventListener('click', () => {
+  let myItem = input.value;
+  input.value = "";
+
+  let listItem = document.createElement("li");
+  let listText = document.createElement("span");
+  let deleteButton = document.createElement("button");
+
+  listItem.appendChild(listText);
+  listText.textContent = myItem;
+  listItem.appendChild(deleteButton);
+  deleteButton.textContent = "❌";
+  list.appendChild(listItem);
+
+  deleteButton.onclick = function(e) {
+    list.removeChild(listItem);
+  };
+
+  input.focus();
+});
